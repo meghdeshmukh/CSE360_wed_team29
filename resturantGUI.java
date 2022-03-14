c2745b089c63dc30d58d1b072ed3b8a614423b16
marcosG01 <81257638+marcosG01@users.noreply.github.com>
2022-03-14 16:51:03 -0700

    Create resturantGUI.java
    initial frame

---
+++ resturantGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class resturantGUI {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Grocery Store");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		login panel = new login();
+		frame.getContentPane().add(new login());
+
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+}
