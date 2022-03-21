104f622a431c5f0d73de1cbcad34a41ba82a63aa
gtapver <99368586+gtapver@users.noreply.github.com>
2022-03-20 21:49:12 -0700

    Update signup.java

--- signup.java
+++ signup.java
@@ -2,8 +2,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 
 
