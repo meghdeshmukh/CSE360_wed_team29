b0e885cf79abf84b75915bcd71578fb19c5471c9
marcosG01 <81257638+marcosG01@users.noreply.github.com>
2022-03-21 20:38:05 -0700

    Update resturantGUI.java

--- resturantGUI.java
+++ resturantGUI.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class resturantGUI {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Grocery Store");
+		JFrame frame = new JFrame("Restaurant");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		login panel = new login();
