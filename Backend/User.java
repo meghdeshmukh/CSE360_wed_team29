832b2ad3fa036b6f00ddc78272f5255396e11964
meghd <meghd@uw.edu>
2022-03-30 21:31:31 -0700

    Minor changes

--- Backend/User.java
+++ Backend/User.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class User {
 	private String email;
 	private String password;
-	
+
 	public User(String email, String password) {
 		this.email = email;
 		this.password = verifyPassword(password);
 	}
-	
-	/* 
+
+	/*
 	 * public Food search(String query) {
 	 * 	Get the menu from application and search through food from menu to find a match of the query
 	 * }
@@ -23,24 +23,24 @@ public class User {
 			Scanner reader = new Scanner(System.in);
 			System.out.println("Password is too Short! Enter a new Password (Password has to be greater than 8 characters): ");
 			String input = reader.nextLine();
+			reader.close();
 			return verifyPassword(input);
 		}
-		
 	}
-	
+
 	public Boolean verifyLogin(String email, String password) {
 		// check database to verify that email and password exit and match
 		return true;
 	}
-	
+
 	public void changeEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getEmail() {
 		return this.email;
 	}
-	
+
 	public void changePassword(String password) {
 		Scanner reader = new Scanner(System.in);
 		System.out.println("Enter your current password: ");
@@ -48,23 +48,24 @@ public class User {
 		if (input.equals(this.password)) {
 			this.password = verifyPassword(password);
 		}
+		reader.close();
 	}
-	
+
 	public void ChangeForcePassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getPassword() {
 		return this.password;
 	}
-	
+
 	/*
 	 * public static void main(String[] args) { User myUser = new
 	 * User("nickfullerton2285@gmail.com", "Slimt");
 	 * System.out.println(myUser.getEmail());
 	 * System.out.println(myUser.getPassword()); myUser.changePassword("bob");
 	 * System.out.println(myUser.getPassword());
-	 * 
+	 *
 	 * }
 	 */
 }
