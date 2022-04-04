938e58fb6890b0cbbbdc9ef21d1d01b81fbdf04d
meghd <meghd@uw.edu>
2022-04-03 17:12:58 -0700

    Valid check added

--- Backend/Payment.java
+++ Backend/Payment.java
@@ -16,15 +16,15 @@ public class Payment {
     private String accountNumber;
     private String cardHolderName;
     private String expireDate;
-    private int cvv;
+    private Integer cvv;
     private String address1;
     private String address2;
     private String city;
     private String state;
     private int zip;
 
-    public Payment(String cardName, String cardType, String accountNumber, String cardHolderName, String expireDate, int cvv, String address1, String address2, String city, String state, int zip) {
-        this.cardName = cardName;
+    public Payment(String cardType, String accountNumber, String cardHolderName, String expireDate, int cvv, String address1, String address2, String city, String state, int zip) {
+        this.cardName = cardType + accountNumber.substring(12,16);
     	this.cardType = cardType;
         this.accountNumber = accountNumber;
         this.cardHolderName = cardHolderName;
@@ -34,100 +34,116 @@ public class Payment {
         this.address2 = address2;
         this.city = city;
         this.state = state;
-        this.zip = zip;
+        this.zip = (Integer)zip;
     }
-    
+
     public Payment(String accountNumber, String cardHolderName, String expireDate, int cvv) {
     	this.accountNumber = accountNumber;
     	this.cardHolderName = cardHolderName;
     	this.expireDate = expireDate;
     	this.cvv = cvv;
     }
-    
+
+    public boolean isValid() {
+        if ((this.accountNumber == null) || (this.accountNumber.length() == 0)) {
+            return false;
+        }
+        if ((this.cardHolderName == null) || (this.accountNumber.length() == 0)) {
+            return false;
+        }
+        if ((this.expireDate == null) || (this.accountNumber.length() == 0)) {
+            return false;
+        }
+        if (this.cvv == null) {
+            return false;
+        }
+        return true;
+    }
+
     public String getCardName() {
     	return this.cardName;
     }
-    
+
     public void setCardName(String cardName) {
     	this.cardName = cardName;
     }
-    
+
     public String getCardType() {
     	return cardType;
     }
-    
+
     public void setCardType(String cardtype) {
     	this.cardType = cardtype;
     }
-    
+
     public String getAccountNumber() {
     	return accountNumber;
     }
-    
+
     public void setAccountNumber(String accNum) {
     	this.accountNumber = accNum;
     }
-    
+
     public String getCardHolderName() {
     	return cardHolderName;
     }
-    
+
     public void setCardHolderName(String cardHolderName) {
     	this.cardHolderName = cardHolderName;
     }
-    
+
     public String getExpireDate() {
     	return expireDate;
     }
-    
+
     public void setExpireDate(String expireDate) {
     	this.expireDate = expireDate;
     }
-    
+
     public int getCVV() {
     	return cvv;
     }
-    
+
     public void setCVV(int cvv) {
     	this.cvv = cvv;
     }
-    
+
     public String getAddressOne() {
     	return address1;
     }
-    
+
     public void setAddressOne(String addressOne) {
     	this.address1 = addressOne;
     }
-    
+
     public String getAddressTwo() {
     	return address2;
     }
-    
+
     public void setAddressTwo(String addressTwo) {
     	this.address2 = addressTwo;
     }
-    
+
     public String getCity() {
     	return city;
     }
-    
+
     public void setCity(String city) {
     	this.city = city;
     }
-    
+
     public String getState() {
     	return state;
     }
-    
+
     public void setState(String state) {
     	this.state = state;
     }
-    
+
     public int getZIP() {
     	return zip;
     }
-    
+
     public void setZIP(int zip) {
     	this.zip = zip;
     }
