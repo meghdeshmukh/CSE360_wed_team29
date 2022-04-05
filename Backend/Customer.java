6dfb186259b420dabc314dfef39b281eafba7716
gtapver <99368586+gtapver@users.noreply.github.com>
2022-04-04 20:16:51 -0700

    replaced Coupon
    replaced Coupon array with array of doubles.
    When checking out, pass in an array of coupons [Doubles] used in the transaction so the checkout functions can remove them from the Customer's account

--- Backend/Customer.java
+++ Backend/Customer.java
@@ -1,3 +1,4 @@
+package Backend;
 import java.util.*;
 
 public class Customer extends User{
@@ -7,7 +8,7 @@ public class Customer extends User{
 	private String name;
 	private Boolean isGuest;
 	private Cart cart;
-	private List<Coupon> coupons;
+	public List<Double> coupons;
 	private List<Payment> payments;
 	private int totalVisits;
 
@@ -38,6 +39,7 @@ public class Customer extends User{
 		this.isGuest = false;
 		this.payments = new ArrayList<Payment>();
 		totalVisits = 0;
+		this.coupons = new ArrayList<Double>();
 	}
 
 	public void changeUsername(String username) {
@@ -68,11 +70,11 @@ public class Customer extends User{
 		return this.isGuest;
 	}
 
-	public void addCoupon(Coupon coupon) {
+	public void addCoupon(Double coupon) {
 		this.coupons.add(coupon);
 	}
 
-	public List<Coupon> getCoupons() {
+	public List<Double> getCoupons() {
 		return this.coupons;
 	}
 
@@ -105,22 +107,31 @@ public class Customer extends User{
 		return totalVisits;
 	}
 
-	public Order customerCheckout(Payment payment) {
+	public Order customerCheckout(Payment payment, List<Double> couponsUsed) {
 		Order order = new Order(this);
 		order.setPayment(payment);
 		order.setPrice(this.cart.getTotal());
 		this.cart = new Cart();
 		totalVisits++;
+		for(Double coupon : couponsUsed) {
+			if(this.coupons.contains(coupon))
+				this.coupons.remove(coupon);
+		}
 		return order;
 	}
 
-	public Order altCheckout(String accountNumber, String cardHolderName, String expireDate, int cvv) {
+	public Order altCheckout(String accountNumber, String cardHolderName, String expireDate, int cvv, List<Double> couponsUsed) {
 		Order order = new Order(this);
 		Payment payment = new Payment(accountNumber, cardHolderName, expireDate, cvv);
 		order.setPayment(payment);
 		order.setPrice(this.cart.getTotal());
 		this.cart = new Cart();
 		totalVisits++;
+		if(!this.getIsGuest())
+			for(Double coupon : couponsUsed) {
+				if(this.coupons.contains(coupon))
+					this.coupons.remove(coupon);
+			}
 		return order;
 	}
 
