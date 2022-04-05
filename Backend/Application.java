8c320e782d154add1cca2e9e586256b0ab117a22
gtapver <99368586+gtapver@users.noreply.github.com>
2022-04-04 20:14:41 -0700

    saveApplication() and restoreApplication()
    saveApplication() will save the Application in its current state to database.txt [In the Backend folder]
    restoreApplication() will set the Application to a state saved in database.txt [In the Backend folder]

--- Backend/Application.java
+++ Backend/Application.java
@@ -1,3 +1,4 @@
+package Backend;
 /*
 Author: Megh Deshmukh in conjunction with CSE 360 Wednesday Team 29
 
@@ -125,5 +126,43 @@ public class Application implements Serializable{
     }
     
     public static void main(String[] args) {
+    	//this will create a test application and save it to a textfile. Run this if the application that was already saved becomes corrupted
+    	Menu testMenu = new Menu();
+    	Application testApp = new Application();
+    	try {
+			testApp.addMenu(testMenu);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	Owner testOwner = new Owner("Admin", "1234");
+    	try {
+			testApp.setOwner(testOwner);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	testApp.addUser(testOwner);
+    	
+    	String[] burgerIng = {"Bread", "Beef", "Tomato", "Lettuce", "Cheese", "Bacon"};
+    	Food burger = new Food("Burger", "burger.png", burgerIng, 11.00, 600, 10);
+    	String[] pizzaIng = {"Bread", "Cheese", "Black Olives", "Bell Pepper", "Pepperoni", "Mushroom"};
+    	Food pizza = new Food("Pizza", "pizza.png", pizzaIng, 18.00, 2500, 20);
+    	String[] pastaIng = {"Pasta", "Tomato Sauce", "Beef", "Basil", };
+    	Food pasta = new Food("Spaghetti", "pasta.png", pastaIng, 9.00, 500, 13);
+    	String[] ratIng = {"Eggplant", "Roma Tomato", "Zucchini", "Squash", "Onion", "Bell Pepper", "Tomato Sauce"};
+    	Food rat = new Food("Ratatouille", "ratatouille.png", ratIng, 15.00, 200, 20);
+    	String[] saladIng = {"Lettuce", "Tomato", "Feta Cheese", "Black Olive", "Cucumber"};
+    	Food salad = new Food("Greek Salad", "salad.png", saladIng, 5.00, 100, 5);
+    	String[] salmonIng = {"Salmon", "Lettuce", "Cucumber", "Tomato", "Lemon", "Avocado"};
+    	Food salmon = new Food("Salmon", "salmon.png", salmonIng, 13.00, 400, 10);
+    	testMenu.add(burger);
+    	testMenu.add(pasta);
+    	testMenu.add(salmon);
+    	testMenu.add(salad);
+    	testMenu.add(pizza);
+    	testMenu.add(rat);
+    	
+    	testApp.saveApplication();
     }
 }
