a0388d56c93447e68408a3349cc6836d93c507f9
gtapver <99368586+gtapver@users.noreply.github.com>
2022-04-04 20:21:08 -0700

    image functionality
    imgPath variable replaced with imgName variable. Save the photo in the images folder [outside Backend], and pass just the name of the image to the parameters. One function will return the name of the image while the other will return a BufferedImage holding the image in question.

--- Backend/Food.java
+++ Backend/Food.java
@@ -1,21 +1,30 @@
+package Backend;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
 /*
 Author: Megh Deshmukh in conjunction with CSE 360 Wednesday Team 29
 
 Code is currently untested and unfinished due to non-integration
 test
 */
-import java.util.*;
-import java.io.Serializable;
 public class Food implements Serializable{
-    private String imgPath;
+    private String imgName;
     private String name;
     private String[] ingredients;
     private Double price;
     private int calories;
     private int timeCook;
 
-    public Food(String name, String imgPath, String[] ingredients, Double price, int calories, int timeCook) {
-        this.imgPath = imgPath;
+    public Food(String name, String imgName, String[] ingredients, Double price, int calories, int timeCook) {
+        this.imgName = imgName;
         this.name = name;
         this.ingredients =  ingredients;
         this.price = price;
@@ -38,12 +47,36 @@ public class Food implements Serializable{
     public int getCalories() {
         return this.calories;
     }
+    
+    public String getImageName(){
+        return imgName;
+    }
 
-    public String getImg() {
-        return this.imgPath;
+    public BufferedImage getImg() {
+        try {
+			return ImageIO.read(Food.class.getResource("../images/" + this.imgName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
     }
 
     public int getTime() {
     	return this.timeCook;
+    }
+    
+    /*
+     * testing to make sure the image finder functions works
+     */
+    public static void main(String[] args) {
+    	
+    	try {
+			//File file = new File("C:\\Users\\gmota\\eclipse-workspace\\CSE360Project\\src\\images\\c.jpg");
+			BufferedImage image = ImageIO.read(Food.class.getResource("../images/salmon.png"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
